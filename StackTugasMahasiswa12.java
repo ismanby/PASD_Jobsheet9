dfa5e5fe930b0ecd444ded7c4ab4fa0016e2fb63
ismanby <ismaanby@gmail.com>
2026-04-24 08:22:35 +0700

    Percobaan 1 - Mahasiswa

---
+++ StackTugasMahasiswa12.java
@@ -0,0 +1,63 @@
+public class StackTugasMahasiswa12 {
+    Mahasiswa12[] stack;
+    int top;
+    int size;
+
+    StackTugasMahasiswa12(int size) {
+        this.size = size;
+        stack = new Mahasiswa12[size];
+        top = -1;
+    }
+
+    boolean isFull() {
+        if (top == size - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void push(Mahasiswa12 mhs) {
+        if (!isFull()) {
+            top++;
+            stack[top] = mhs;
+        } else {
+            System.out.println("Stack penuh! Tidak bisa menambahkan tugas lagi.");
+        }
+    }
+
+    public Mahasiswa12 pop() {
+        if (!isEmpty()) {
+            Mahasiswa12 m = stack[top];
+            top--;
+            return m;
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas untuk dinilai.");
+            return null;
+        }
+    }
+
+    public Mahasiswa12 peek() {
+        if (!isEmpty()) {
+            return stack[top];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan.");
+            return null;
+        }
+    }
+
+    void print() {
+        for (int i = 0; i <= top; i++) {
+            System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
+        }
+        System.out.println("");
+    }
+} 
